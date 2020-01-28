649c3cebf364c17cbdb90a4f95295a9ab0860f36
Samuel Escobar <sescobar8714@gmail.com>
2020-01-28 04:14:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,12 +4,13 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{
+      :hero_friends => [
+        {
       :name => "Benvolio",
       :age => "17",
       :attitude => "worried"
     },
-    {
+       {
       :name => "Mercutio",
       :age => "18",
       :attitude => "hot-headed"
@@ -33,7 +34,4 @@ def fourth_challenge
       ]
    }
 }
-
-
-
 end
